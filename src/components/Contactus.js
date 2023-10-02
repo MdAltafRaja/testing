1a0eb665fbd735e534df7b52c42b8421f632e14e
Altaf Raja <mdaltafraja41@gmail.com>
2023-10-02 21:46:40 +0530

    deploying on github

---
+++ src/components/Contactus.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Contactus() {
+  return (
+    <div>
+      <Navbar />
+      Contactus Data
+    </div>
+  );
+}
+
+export default Contactus;
